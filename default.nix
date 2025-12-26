f0ae538a39a9ee92c159253822040f338c55b418
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2025-12-25 17:35:24 -0800

    Get nix ready for multiple specs.

--- default.nix
+++ default.nix
@@ -1,17 +1,25 @@
 let
   pkgs = import ./pinned.nix;
-in
-  pkgs.runCommand
-    "spec"
-    {
-      buildInputs = [ pkgs.tlaplus ];
-    }
-    ''
-    set -euo pipefail
 
-    SPEC=intersection_abstract.tla
-    WORKERS=$(( $(nproc) * 3 / 4 ))
-    cp -L ${./intersection_abstract.tla} $SPEC
-    mkdir $out
-    tlc $SPEC -config ${./intersection_abstract.cfg} -workers $WORKERS | tee $out/$SPEC.log
-    ''
+  intersection_abstract =
+    pkgs.runCommand
+      "intersection_abstract"
+      {
+        buildInputs = [ pkgs.tlaplus ];
+      }
+      ''
+      set -euo pipefail
+
+      SPEC=intersection_abstract.tla
+      WORKERS=$(( $(nproc) * 3 / 4 ))
+      cp -L ${./intersection_abstract.tla} $SPEC
+      mkdir -p $out/share
+      tlc $SPEC -config ${./intersection_abstract.cfg} -workers $WORKERS | tee $out/share/$SPEC.log
+      '';
+in
+  pkgs.symlinkJoin
+    { name = "all_specs";
+      paths =
+        [ intersection_abstract
+        ];
+    }
